1ca301a4df365357c7a1e269cc9755c737ccb1e4
Oleg Kudimov <ovkudimov@kai.ru>
2020-10-23 01:03:38 +0300

    fixed scroll bug in cell

--- AppStore/Controlles/Today/AppFullScreenController.swift
+++ AppStore/Controlles/Today/AppFullScreenController.swift
@@ -18,25 +18,41 @@ class AppFullScreenController: UITableViewController {
     }
     
     
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = TodayCell()
-        return header
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 450
-    }
+//    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let header = TodayCell()
+//        return header
+//    }
+//
+//
+//    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return 450
+//    }
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = AppFullScreenDescriptionCell()
         
+        if indexPath.item == 0 {
+            let cell = UITableViewCell()
+            let todayCell = TodayCell()
+            cell.addSubview(todayCell)
+            todayCell.centerInSuperview(size: .init(width: 250, height: 250))
+            cell.selectionStyle = .none
+            return cell
+        }
+        
+        
+        let cell = AppFullScreenDescriptionCell()
+        cell.selectionStyle = .none
         return cell 
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 450
     }
 }
