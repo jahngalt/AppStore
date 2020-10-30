5eadabe6973bd77a13c1189cb9c8ecb88b86df87
Oleg Kudimov <ovkudimov@kai.ru>
2020-10-30 12:48:11 +0300

    added cell separator view

--- AppStore/Views/Today/MultipleAppCell.swift
+++ AppStore/Views/Today/MultipleAppCell.swift
@@ -25,6 +25,12 @@ class MultipleAppCell: UICollectionViewCell {
     let getButton = UIButton(title: "GET")
     let nameLabelSize: CGFloat = 16
     
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.3, alpha: 0.3)
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -48,6 +54,9 @@ class MultipleAppCell: UICollectionViewCell {
         stackView.alignment = .center
         addSubview(stackView)
         stackView.fillSuperview()
+        //adding live separator between cells
+        addSubview(separatorView)
+        separatorView.anchor(top: nil, leading: nameLabel.leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: -6, right: 0), size: .init(width: 0, height: 0.2))
     }
     
     required init?(coder: NSCoder) {
